9f480f54b0226f679b81ddf0bd22a8a39b775ef6
lazzben <458861463@qq.com>
2020-01-12 11:43:48 +0800

    添加css

--- webpack.config.js
+++ webpack.config.js
@@ -15,14 +15,18 @@ module.exports = {
   },
   module: {
     rules: [
-      { 
-        test: /\.tsx?$/, 
-        loader: 'awesome-typescript-loader' 
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
       },
       {
         test: /\.svg$/,
         loader: 'svg-sprite-loader',
       },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
     ]
   },
 }
