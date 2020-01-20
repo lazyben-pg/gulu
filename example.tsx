7f69ac56263f5b462ebbcacb8b764b217da66afd
lazzben <458861463@qq.com>
2020-01-20 14:50:43 +0800

    add exmple router

--- example.tsx
+++ example.tsx
@@ -1,5 +1,29 @@
 import React from "react"
 import ReactDOM from 'react-dom'
-import Icon from './lib/icon/icon'
+import { HashRouter as Router, Link,Route } from 'react-router-dom'
 
-ReactDOM.render(<Icon name="wechat"/>, document.querySelector('#root'))
+import Exampleicon from './lib/icon/exampleicon'
+import Examplebutton from './lib/button/examplebutton'
+
+ReactDOM.render(
+  <Router>
+    <header>
+      <div className='logo'>
+        gulugulu
+      </div>
+    </header>
+    <div>
+      <aside>
+        <h2>组件</h2>
+        <ul>
+          <li><Link to='/icon'>Icon</Link></li>
+          <li><Link to='/button'>Button</Link></li>
+        </ul>
+      </aside>
+      <main>
+        <Route path="/icon" component={Exampleicon}/>
+        <Route path="/button" component={Examplebutton}/>
+      </main>
+    </div>
+  </Router>
+  , document.querySelector('#root'))
