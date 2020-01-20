393edaa48bd5293fd329408f8bb7988845de835d
lazzben <458861463@qq.com>
2020-01-20 14:21:26 +0800

    add example

---
+++ example.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+import ReactDOM from 'react-dom'
+import Icon from './lib/icon/icon'
+
+ReactDOM.render(<Icon name="wechat"/>, document.querySelector('#root'))
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,9 +3,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = Object.assign({}, base, {
   mode: 'development',
+  entry: {
+    example: './example.tsx'
+  },
   plugins: [new HtmlWebpackPlugin({   //使index.html 自动引入 index.js
     title: 'gulu',
-    template: 'index.html'
+    template: 'example.html'
   })
   ]
 })
